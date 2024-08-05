3c5f2f28140cd184fa61361987441cf801bbf9e9
User <User@DESKTOP-U96UIA2>
2024-08-05 20:34:28 +0200

    Add project files.

---
+++ Project2-TechTrendsAPI-38342626/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Project2_TechTrendsAPI_38342626
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
